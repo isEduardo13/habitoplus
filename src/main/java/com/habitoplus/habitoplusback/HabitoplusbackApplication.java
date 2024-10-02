ddaa8b9bbc6bf823a98b653c4a4b5cc71f2b881b
EduardoH <eduardoisidroh13@gmail.com>
2024-10-02 15:21:56 -0600

    first commit

---
+++ src/main/java/com/habitoplus/habitoplusback/HabitoplusbackApplication.java
@@ -0,0 +1,13 @@
+package com.habitoplus.habitoplusback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitoplusbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitoplusbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habitoplus/habitoplusback/HabitoplusbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitoplus.habitoplusback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitoplusbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
