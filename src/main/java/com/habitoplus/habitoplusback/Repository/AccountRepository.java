014a681674533e50d8b8f306b85ce1ff33ed5340
isEduardo13 <eduardoisidroh13@gmail.com>
2024-10-02 22:33:25 -0600

    Refactor AccountController, Account, AccountRepository, and AccountService classes

--- src/main/java/com/habitoplus/habitoplusback/Repository/AccountRepository.java
+++ src/main/java/com/habitoplus/habitoplusback/Repository/AccountRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    Account findByEmail(String email);
+    
 }
