91082a3dc7aeec86c16ab9698ab41054e8ebfb93
EduardoH <eduardoisidroh13@gmail.com>
2024-10-02 20:50:54 -0600

    Refactor Account and AccountRepository classes

--- src/main/java/com/habitoplus/habitoplusback/Model/Account.java
+++ src/main/java/com/habitoplus/habitoplusback/Model/Account.java
@@ -1,12 +1,45 @@
 package com.habitoplus.habitoplusback.Model;
 
+import org.springframework.data.annotation.AccessType;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Account{
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long account_id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_id", referencedColumnName = "profile_id")
+    private Profile profile;
+
+    @Column(unique = true, nullable = false, name = "email")
+    private String email;
+
+    @Column(nullable = false, name = "password")
+    private String password;
+
+    @Column(nullable = false, name = "status", columnDefinition = "boolean default false")
+    private boolean status;
+
+    public Account() {
+    }
 
 
 
+    public Account(Profile profile, String email, String password, boolean status) {
+        this.profile = profile;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+    }
 }
 
--- src/main/java/com/habitoplus/habitoplusback/Repository/AccountRepository.java
+++ src/main/java/com/habitoplus/habitoplusback/Repository/AccountRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 @Repository
-public interface AccoutRepository extends JpaRepository<Account, Long> {
-    
+public interface AccountRepository extends JpaRepository<Account, Long> {
 
 }
