dd88bfb9486200a181c1fefdf46c178c5a6cce3f
isEduardo13 <eduardoisidroh13@gmail.com>
2024-10-02 22:43:03 -0600

    Refactor ExeptionHandlerAdvice class and add exception handling for NoSuchElementException

--- src/main/java/com/habitoplus/habitoplusback/Exeption/ExeptionHandlerAdvice.java
+++ src/main/java/com/habitoplus/habitoplusback/Exeption/ExeptionHandlerAdvice.java
@@ -1,14 +1,17 @@
 package com.habitoplus.habitoplusback.Exeption;
 
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExeptionHandlerAdvice {
-    @ExceptionHandler(NotFoundException.class)
-    public  String handleNotFound(NotFoundException e){
-        return e.getMessage();
+    @ExceptionHandler(NoSuchElementException.class) 
+    public ResponseEntity<?>handleException(NoSuchElementException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Element not found");
     }
+   
 
 }
