6e1e2a72672dcee54e465396b37844ba363092ed
EduardoH <eduardoisidroh13@gmail.com>
2024-10-04 13:27:21 -0600

    Refactor ProfileController to make getAllProfiles() method public

--- src/main/java/com/habitoplus/habitoplusback/Model/Account.java
+++ src/main/java/com/habitoplus/habitoplusback/Model/Account.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.Transient;
 
 @Entity
 public class Account{
--- src/main/java/com/habitoplus/habitoplusback/Model/Profile.java
+++ src/main/java/com/habitoplus/habitoplusback/Model/Profile.java
@@ -1,11 +1,8 @@
 package com.habitoplus.habitoplusback.Model;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Transient;
 
@@ -17,16 +14,16 @@ public class Profile {
 
     @OneToOne(mappedBy = "profile")
     private Account account;
+
+
+    @Transient
+    private Photo photo;
+
+
     public int getIdProfile() {
         return idProfile;
     }
 
-        // Almacena solo el ID de la foto
-        private String idPhoto;
-
-        // Campo transitorio para el objeto Photo
-        @Transient
-        private Photo photo;
 
     public void setIdProfile(int idProfile) {
         this.idProfile = idProfile;
